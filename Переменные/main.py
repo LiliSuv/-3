931f28e12a0ac233c62ccd60155298a69bce9454
LiliSuv <faufy@yandex.ru>
2024-07-10 03:28:15 +0300

    Add files via upload

---
+++ Переменные/main.py
@@ -0,0 +1,5 @@
+course='Python'
+completed_tasks='12'
+time_spent='1.5'
+time_of_one_task=float(time_spent)/float(completed_tasks)
+print( 'Курс:', course,', всего задач:', completed_tasks,', затрачено часов:', time_spent, ', среднее время выполнения одного задания:', time_of_one_task, 'часа')
